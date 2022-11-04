7266f9d43c913b38dbd2a212901ec03513e8f54d
aldimenur <aldimenur@gmail.com>
2022-11-04 17:39:21 +0700

    instal redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Grid from "@mui/material/Grid";
+import Container from "@mui/system/Container";
 import "./App.css";
 import AddTodo from "./components/AddTodo";
 import TodoLists from "./components/TodoLists";
@@ -6,17 +7,19 @@ import TotalItems from "./components/TotalItems";
 
 function App() {
   return (
-    <div className="Container">
-      <Grid container justifyContent={"center"} spacing={2}>
-        <Grid item lg={4} xs={10}>
-          <AddTodo />
+    <Container maxWidth="lg">
+      <div className="Container">
+        <Grid container justifyContent={"center"} spacing={2}>
+          <Grid item lg={4} xs={10}>
+            <AddTodo />
+          </Grid>
+          <Grid item lg={6} xs={10}>
+            <TodoLists />
+            <TotalItems />
+          </Grid>
         </Grid>
-        <Grid item lg={6} xs={10}>
-          <TodoLists />
-          <TotalItems />
-        </Grid>
-      </Grid>
-    </div>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+  reducer: {},
+});
