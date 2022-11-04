e0e409da323fc6f8b622c1836e9d95c75bbbb0c0
aldimenur <aldimenur@gmail.com>
2022-11-04 14:49:17 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <div className="box__app">
+        <div className="box__title">
+          <h1 style={{ color: "#E26868" }}>Simple Todo App</h1>
+        </div>
+      </div>
     </div>
   );
 }
